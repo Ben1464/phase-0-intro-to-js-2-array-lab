2d7568b9a216825bbe0b7b4ba26e615ee6a825d3
Benson Mwangi <kimaniben@gmail.com>
2023-10-03 08:17:00 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-// Write your solution here!
-beforeEach(function () {
-    cats.length = 0;
-  
-    cats.push("Milo", "Otis", "Garfield");
-  });
+let cats =  ['Milo','Otis','Garfield'];
+function destructivelyAppendCat(){
+  cats.push('Ralph');
+}
+function destructivelyPrependCat(){
+  cats.unshift('Bob');
+}
+function destructivelyRemoveLastCat(){
+  cats.pop('Bob');
+}
+function destructivelyRemoveFirstCat(){
+  cats.shift('Bob');
+}
+function appendCat(name){
+  const newCats = [...cats, name];
+  return newCats;
+}
+
+function prependCat(name){
+  const newCats = [name, ...cats]
+  return newCats;
+}
+
+function removeLastCat(){
+  const newCats = cats.slice(0,-1)
+  return newCats;
+}
+
+function removeFirstCat(){
+  const newCats = cats.slice(1);
+  return newCats;
+}
